b91af8213c81eac41b269493a95022e038073672
vpulav2 <vpulav2@pal-achieve-03.ececs.uic.edu>
2024-06-18 23:12:48 -0500

    [UPDATE]: added JASPER GPT results

--- old_results_not_of_concern/usbf_pa/sim_command_usbf_pa.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iverilog -g2001 -osimv -s usbf_pa_bench /data/vpulav2/Work/GoldMine/Runtime/goldmine.out/usbf_pa/usbf_pa_bench.v /data/vpulav2/Work/GoldMine/verilog/usb/usbf_pa.v  -I/data/vpulav2/Work/GoldMine/verilog/usb
--- old_results_not_of_concern/usbf_pd/sim_command_usbf_pd.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-iverilog -g2001 -osimv -s usbf_pd_bench /data/vpulav2/Work/GoldMine/Runtime/goldmine.out/usbf_pd/usbf_pd_bench.v /data/vpulav2/Work/GoldMine/verilog/usb/usbf_pd.v  -I/data/vpulav2/Work/GoldMine/verilog/usb
